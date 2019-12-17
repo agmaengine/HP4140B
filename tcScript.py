435f7850bb7349b3a0bce1fee6d22c08ddc8f6a1
user@plasmalab.physics.chula <user@server>
2019-12-17 21:21:41 +0700

    fine tune

--- tcScript.py
+++ tcScript.py
@@ -6,15 +6,16 @@ This is a temporary script file.
 """
 #%% include
 import pyvisa
-import time
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 #%% estrablish remote instrument 
 
 rm = pyvisa.ResourceManager()
 # GPIB Address
-addr = str(16)
+addr = str(9)
 # Create instrument object GPIB Protocol
 tc = rm.open_resource('GPIB0::'+addr+'::INSTR')
 
@@ -34,16 +35,17 @@ tc = rm.open_resource('GPIB0::'+addr+'::INSTR')
 # set Range to auto
 # set auto range limit to 10-5 A
 # set integration time to long
-print('initialize instruments')
-tc.write('F2T1L3A1RA1H05I3')
-print('initialized')
+def initialize():
+    print('initialize instruments')
+    tc.write('T1L3A1RA1H05I3')
+    print('initialized')
 #%% sweep setup
-def sweep_setup(vstart,vstop,step,rcV=1):
+def continuous_sweep_setup(vstart,vstop,step,rcV=1):
     #   vstart (volt)
     #   vstop (volt) must > vstart
     #   step (volt) must < vstop - vstart
     #   rcV (V/s) rate of change of voltage
-    
+    tc.write('F2')
     # set VA to 0V
     tc.write('PA0')
     # set VA start value = -5V
@@ -55,10 +57,32 @@ def sweep_setup(vstart,vstop,step,rcV=1):
     # set step dV/dt to 1V/s
     tc.write('PV%.1f'%rcV)
 
+def highres_sweep_setup(vstart,vstop,step):
+    tc.write('F1')
+    tc.write('PA0')
+    # set VA start value = -5V
+    tc.write('PS%.1f'%vstart)
+    # set VA stop value = 5V
+    tc.write('PT%.1f'%vstop)
+    # set voltage change step to 1V
+    tc.write('PE%.1f'%step)
+    # set hold time 0.2sec
+    #tc.write('PH0.1')
+    # set step delay time 0.15s
+    tc.write('PD0.15')
+    # set step dV/dt to 0V/s
+    tc.write('PV0')
+
 print('setting sweep parameters')
-sweep_setup(-10.1,10.1,0.1)
+initialize()
+#continuous_sweep_setup(-100,100,0.1)
+Vstart = -100
+Vstop = 100
+highres_sweep_setup(Vstart,Vstop,0.1)
 print('set')
 #%% sweep
+I = []
+V = []
 print('collecting data')
 tc.write('W1')
 trigger = True
@@ -69,33 +93,34 @@ while trigger:
         IV.append(tc.read_bytes(23).decode())
     except:
         trigger = False
+    if IV[-1][0:2] == ' N':
+        im1, im2 = IV[-1].split(',')
+        # split record data 
+        I.append(float(im1[3:]))
+        V.append(float(im2[1:]))
+    if V[-1] == float(Vstop):
+        trigger =False
     # wait for the device to make the next measurements for 50 ms
     # time.sleep(0.02)
 
 # record data in SI unit
-
-#%% data processing
-
-I = []
-V = []
-for i in range(len(IV)):
-    # filter read data with no data
-    if IV[i][0:2] == ' N':
-        im1, im2 = IV[i].split(',')
-        # split record data 
-        I.append(float(im1[3:]))
-        V.append(float(im2[1:]))
-
 # turn I,V to numpy
         
 I = np.array(I)
 V = np.array(V)
 print('data collected')
-#%% plot
+#plot
 print('visualize data')
-plt.plot(V,I*1e3,'-o')
+plt.plot(V,I*1e6,'-o')
 plt.xlabel('Volt (V)')
-plt.ylabel('Current (mA)')
+plt.ylabel('Current (uA)')
 plt.title('I-V characteristics')
 print('done!')
+#%% save
+VI = pd.DataFrame(np.array([V,I]).T,columns=['V(V)','I(A)'])
+t = datetime.datetime.now()
+timestr = str(t.year)+'%02d'%t.month+'%02d'%t.day+'_'+'%02d'%t.hour+'%02d'%t.minute
+VI.to_csv('../../DataLogs/'+timestr+'.csv',index=False)
+print(timestr)
+
 #%% functions
