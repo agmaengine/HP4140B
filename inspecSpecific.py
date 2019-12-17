8dd1a803e522813c935b4d3314da60c0eac2824d
user@plasmalab.physics.chula <user@server>
2019-12-17 21:29:04 +0700

    tune

--- inspecSpecific.py
+++ inspecSpecific.py
@@ -12,33 +12,24 @@ import pandas as pd
 
 #%%
 def pdIVplot(iv):
-    plt.plot(iv['V(V)'].to_numpy(),iv['I(mA)'].to_numpy()*1e6)
+    plt.plot(iv['V(V)'].to_numpy(),iv['I(A)'].to_numpy()*1e6)
 
 #%% loads data
-Exp = pd.read_excel('../../LangmuirExp/2019-12-13/TraceMeta.xlsx',index_col=0)
+namelist = ['20191217_1820','20191217_1833']
 
-# sorted respected to Vr with the first at the end to pop it out
-# when loop initiate
-namelist = Exp['IV_Filename'][[4,1,2,3,5]].to_list()
-
-Vr=-100
-iv = pd.read_csv('../../DataLogs/'+namelist.pop()+'.csv')
-iv['V(V)']=iv['V(V)']+Vr
+iv=[]
+iv.append(pd.read_csv('../../DataLogs/'+namelist.pop()+'.csv'))
 fig = plt.figure()
-pdIVplot(iv)
-Vr=Vr+50
+pdIVplot(iv[0])
 
 for name in namelist:
-    Inter = pd.read_csv('../../DataLogs/'+name+'.csv')
-    Inter['V(V)']=Inter['V(V)']+Vr
-    pdIVplot(Inter)
-    iv = iv.append(Inter)
-    Vr = Vr + 50
-
+    iv.append(pd.read_csv('../../DataLogs/'+name+'.csv'))
+    pdIVplot(iv[-1])
+    
 x = np.arange(-200,200)
 plt.plot(x,np.zeros(len(x)))
 plt.xlabel('V (V)')
-plt.ylabel('I (nA)')
+plt.ylabel('I (uA)')
 plt.grid()
 #plt.plot(V,I)
 #%%
